f9ef69cf51c55af4ca31744306e78c066caf85dd
lihao <hao873835932>
2019-07-23 20:10:23 +0800

    dinshi

--- src/main/java/com/readbook/book/FqController.java
+++ src/main/java/com/readbook/book/FqController.java
@@ -412,30 +412,30 @@ public class FqController {
                     //  sb.append(lineTxt + "\n");
                 }
                 System.out.println(okNodeList);
-                ExecutorService executorServiceCN2 = Executors.newFixedThreadPool(500);
+           //     ExecutorService executorServiceCN2 = Executors.newFixedThreadPool(500);
 
                 //判断是否cn2
                 Set<SSRNode> isCN2set = new HashSet<>();
 
                 for (SSRNode sn : okNodeList) {
-                    executorServiceCN2.submit(
+                  /*  executorServiceCN2.submit(
                             new Thread(new Runnable() {
                                 @Override
                                 public void run() {
-
+*/
                                     Boolean isCN2 = TraceIP.traceip(sn.getServer());
                                     if (isCN2) {
                                         isCN2set.add(sn);
                                     }
-                                }
+                              /*  }
                             })
-                    );
+                    );*/
 
                 }
-                executorServiceCN2.shutdown();
+               // executorServiceCN2.shutdown();
 
                 //等待所有任务都执行结束
-                if (executorServiceCN2.isTerminated()) {
+               // if (executorServiceCN2.isTerminated()) {
                     System.out.println("cn2判断完毕");
                     String shuchu = "";
                     for (SSRNode sn : isCN2set) {
@@ -444,7 +444,7 @@ public class FqController {
                     }
                     createFile("isCN2", shuchu);
 
-                }
+                //}
                 read.close();
             } else {
                 System.out.println("找不到指定的文件");
---
+++ src/main/java/com/readbook/book/ssr/SsrQuartz.java
@@ -0,0 +1,15 @@
+package com.readbook.book.ssr;
+import java.util.Date;
+import org.quartz.DisallowConcurrentExecution;
+import org.quartz.Job;
+import org.quartz.JobDetail;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+public class SsrQuartz implements Job  {
+    public void execute(JobExecutionContext context) throws JobExecutionException {
+        JobDetail detail = context.getJobDetail();
+        String name = detail.getJobDataMap().getString("name");
+        System.out.println("HHH:"+name);
+    }
+}
